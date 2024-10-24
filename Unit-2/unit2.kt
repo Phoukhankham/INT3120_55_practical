edb62f24e962634a012e2601b4788f87ee2067ab
21021680Phoukhankham <21021680@vnu.edu.vn>
2024-10-24 15:47:33 +0700

    Water Me

---
+++ Unit-2/unit2.kt
@@ -0,0 +1,38 @@
+#If else Statement
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}
+# When statement
+fun main() {
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+# If else and When 
+fun main() {
+    val trafficLightColor = "Amber"
+
+    val message = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+    println(message)
+}
